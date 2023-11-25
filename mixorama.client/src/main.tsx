0b7b8ef48b10e86930016c47cc6a9beb2b2db282
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2023-11-25 11:40:14 +0100

    Added Auth0Provider as a wrapper for the React app.

--- mixorama.client/src/main.tsx
+++ mixorama.client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+        domain="dev-rglgi3ifuoda0lfd.eu.auth0.com"
+        clientId="ciWiedZqOuIQM3q0IkCO7QXV1TMJIkco"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}>
+        <App />
+    </Auth0Provider>,
+);
