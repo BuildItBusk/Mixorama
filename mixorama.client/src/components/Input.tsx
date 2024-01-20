a5beee0ce87f26afd42b1ae25af7e2e1e47e0601
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2024-01-20 21:57:10 +0100

    Can now add/remove rows of ingredients.

--- mixorama.client/src/components/Input.tsx
+++ mixorama.client/src/components/Input.tsx
@@ -13,7 +13,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({ inputName, placeholder
         placeholder={placeholder}
         {...rest} 
         ref={ref}
-        className="block w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block w-full h-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
   );
 });
--- mixorama.client/src/routes/CreateCocktail.tsx
+++ mixorama.client/src/routes/CreateCocktail.tsx
@@ -18,20 +18,27 @@ interface Ingredient {
 }
 
 const CreateCocktail = () => { 
-    const {register, getValues, handleSubmit} = useForm<Cocktail>({ 
+    const {register, getValues, setValue, watch, handleSubmit} = useForm<Cocktail>({ 
         defaultValues: { 
             name: '',
             description: '',
             imageUrl: '',
             ingredients: [
-                {name: 'Lys rom', quantity: '', unit: ''}
+                {name: '', quantity: '', unit: ''}
             ]
         } 
     });
 
+    const ingridientsList = watch('ingredients')
+
     const onAddIngredient = () => {
         console.log("Adding ingredient");
-        getValues().ingredients.push({name: '', quantity: '', unit: ''});
+        setValue('ingredients', [...getValues('ingredients'), {name: '', quantity: '', unit: ''}])
+    }
+
+    const onRemoveIngredient = () => {
+        console.log("Removing ingredient");
+        setValue('ingredients', [...getValues('ingredients').slice(0, -1)])
     }
     
     const onSubmit: SubmitHandler<Cocktail> = async data => {
@@ -58,7 +65,7 @@ const CreateCocktail = () => {
             console.log(error);
         }
     };
-      
+    
     return (
         <>
             <div className="flex items-center justify-center content-start w-full">
@@ -89,24 +96,31 @@ const CreateCocktail = () => {
                         <Label htmlFor="ingredients" text="Ingredienser" />
                         
                         <div  className="flex flex-col space-y-3">
-                            {getValues().ingredients.map(() => (
+                            {ingridientsList.map(() => (
                             <div key={Math.random()} className="flex flex-row space-x-3">
-                                <input
-                                    type="text"
-                                    placeholder="Lys rom" 
-                                    className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" />
-                                
-                                <input 
-                                    type="text" 
-                                    placeholder="60" 
-                                    className="block w-1/3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" />
-
-                                <select
-                                    className="w-1/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option value="ml">ml</option>
-                                    <option value="dashes">stænk</option>
-                                    <option value="stk">stk</option>    
-                                </select>                 
+                                <div className="w-full">
+                                    <Input
+                                        inputName="ingredientName"
+                                        placeholder="Lys rom" />
+                                </div>
+                                <div className="w-1/3">
+                                <Input 
+                                    inputName="ingredientQuantity"
+                                    placeholder="60" />
+                                </div>
+                                <div className="w-1/3">
+                                    <select
+                                        className="w-1/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option value="ml">ml</option>
+                                        <option value="dashes">stænk</option>
+                                        <option value="stk">stk</option>    
+                                    </select>
+                                </div>
+                                <button type="button" className="p-0 w-12 bg-transparent" onClick={onRemoveIngredient}>
+                                    <div className="flex items-center">
+                                        <img src="trash-icon.png"/>
+                                    </div>
+                                </button>
                             </div>
                             ))} 
                             <div>
